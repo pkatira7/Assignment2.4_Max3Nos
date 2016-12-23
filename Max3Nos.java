ff008b114167128dd23f7f8299dc7ffd3f4e56e4
pkatira7 <pkatira@vmware.com>
2016-12-23 15:47:57 +0530

    Add files via upload

---
+++ Max3Nos.java
@@ -0,0 +1,28 @@
+
+public class Max3Nos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int i =7,j=5,k=4;
+      if (i > j)
+      {
+    	 if ( i > k )
+    		System.out.println("I is the highest");
+    	 else 
+    	 { 
+    		 System.out.println("K is the highest");
+    	 }
+    	 
+      }
+    else 
+    	 {
+    		 if ( j > k )
+    	    		System.out.println("J is the highest");
+    	    	 else { 
+    	    		 System.out.println("K is the highest");
+    	 }
+      }
+      
+	}
+
+}
